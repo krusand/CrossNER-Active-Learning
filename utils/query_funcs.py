0a1c8422a42cdd55897c32a6e5165389a5b886d0
Joke Heinks <37629640+jokeheinks@users.noreply.github.com>
2024-04-15 14:06:17 +0200

    Changed queries to argmax instead of sum

--- utils/query_funcs.py
+++ utils/query_funcs.py
@@ -17,14 +17,38 @@ def random_query(data_loader, query_size = 10):
     return indexes[0:query_size]
 
 def least_confidece(model, device, data_loader, query_size = 10):
+    """Returns indices for the data with the lowest confidence
+
+    Args:
+        model (model_new.BertForTokenClassification): Model used for prediction
+        device (torch.device): Pytorch device
+        data_loader (torch.utils.data.dataloader.DataLoader): DataLoader with random sampler
+        query_size (int, optional): Number of elements to return. Defaults to 10.
+
+    Returns:
+        np.array: Array of indeces
+    """
     logits, mask, indices = model.predict(data_loader, device)
     probabilities = [F.softmax(r, dim = 1) for r in logits]
+
     confidences = [torch.max(r, dim=1).values for r in probabilities] 
-    summed_confidences = [torch.dot(c, m.float()).cpu() for c,m in zip(confidences, mask)]
+    summed_confidences = [torch.multiply(c, m.float()).cpu().max() for c,m in zip(confidences, mask)]
+
     sorted_idx = np.argsort(summed_confidences)
     return np.asarray(indices)[sorted_idx][0:query_size]
 
 def margin_of_confidence(model, device, data_loader, query_size):
+    """Returns indices for the data with the lowest margin of confidence
+
+    Args:
+        model (model_new.BertForTokenClassification): Model used for prediction
+        device (torch.device): Pytorch device
+        data_loader (torch.utils.data.dataloader.DataLoader): DataLoader with random sampler
+        query_size (int, optional): Number of elements to return. Defaults to 10.
+
+    Returns:
+        np.array: Array of indeces
+    """
     logits, mask, indices = model.predict(data_loader, device)
 
     probabilities = [F.softmax(sentence, dim = 1) for sentence in logits]
@@ -32,10 +56,10 @@ def margin_of_confidence(model, device, data_loader, query_size):
     toptwo = [torch.topk(sentence, 2, dim=1)[0] for sentence in probabilities]
     difference = [torch.abs(sentence[:,0]-sentence[:,1]) for sentence in toptwo]
 
-    summed_margins = [torch.dot(c, m.float()).cpu() for c,m in zip(difference, mask)]
+    summed_margins = [torch.multiply(c, m.float()).cpu().max() for c,m in zip(difference, mask)]
 
     sorted_idx = np.argsort(summed_margins)
-    
+
     return np.asarray(indices)[sorted_idx][0:query_size]
 
 def ratio(model, device, data_loader, query_size):
